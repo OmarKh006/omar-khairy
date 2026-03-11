d44b9df92ca39d85aaacae9412cf9ed866ee6b65
OmarKh006 <khairyomar006@gmail.com>
2026-03-12 00:01:41 +0200

    changing of the DOM Structure of the skills component

--- ch1.js
+++ ch1.js
@@ -1,1 +1,15 @@
-console.log("test");
+const editedRedux = document.querySelector("[data-name=redux] .skills__title");
+const editedReactTestingLibrary = document.querySelector(
+  "[data-name='react testing library'] .skills__title",
+);
+const js = document.querySelector("[data-name=javascript]");
+const typescript = `
+            <li data-name="typescript" class="skills__item">
+                <h2 class="skills__title">Typescript</h2>
+                <span class="skills__years">2 years</span>
+            </li>
+`;
+
+editedRedux.innerText = "Node.js";
+editedReactTestingLibrary.innerText = "MongoDB";
+js.insertAdjacentHTML("afterend", typescript);
